d81e9f6d67fb916ddf50579dfd3cfb29107b205c
Daniel Sánchez <dev@daniromo.me>
2022-08-29 14:43:50 -0500

    Initialized android project and made a logo 🎨

---
+++ android/app/src/main/java/me/daniromo/eva/MainActivity.java
@@ -0,0 +1,5 @@
+package me.daniromo.eva;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'me.daniromo.eva',
   appName: 'eva',
   webDir: 'www',
   bundledWebRuntime: false
