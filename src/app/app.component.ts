fab6221d307c651ba92816c50e02c2859143ee4a
Daniel Sánchez <dev@daniromo.me>
2022-09-02 17:34:44 -0500

    Tweaked app behaviour 🍙

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,36 +17,36 @@ export class AppComponent {
     { title: 'Archived', url: '/folder/Archived', icon: 'archive' },
     { title: 'Trash', url: '/folder/Trash', icon: 'trash' },
     { title: 'Spam', url: '/folder/Spam', icon: 'warning' },
-  ];
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
-  public initialized$: Observable<boolean>;
+  ]
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders']
+  public initialized$: Observable<boolean>
   public splash: AnimationOptions = {
     path: 'assets/eva.json',
     loop: false
   };
-  private initialized: BehaviorSubject<boolean>;
+  private initialized: BehaviorSubject<boolean>
   constructor(
     private platform: Platform
   ) {
-    this.initialized = new BehaviorSubject(false);
-    this.initialized$ = this.initialized.asObservable();
-    this.init();
+    this.initialized = new BehaviorSubject(false)
+    this.initialized$ = this.initialized.asObservable()
+    this.init()
   }
 
   private async init() {
-    await this.platform.ready();
-    if (this.platform.is('ios')) await this.showSplash();
-    this.initialized.next(true);
+    await this.platform.ready()
+    if (this.platform.is('ios') && this.platform.is('hybrid')) await this.showSplash()
+    this.initialized.next(true)
     GoogleAuth.initialize({
       clientId: '733249240859-k9viv6v3lagtbqn68uk0a4mnk52ng5h5.apps.googleusercontent.com',
       scopes: ['profile', 'email'],
       grantOfflineAccess: true,
-    });
+    })
   }
 
   private async showSplash() {
-    const lottie = (window as any).lottie;
-    await lottie.splashscreen.hide();
-    await lottie.splashscreen.show('public/assets/eva.json', false);
+    const lottie = (window as any).lottie
+    await lottie.splashscreen.hide()
+    await lottie.splashscreen.show('public/assets/eva.json', false)
   }
 }
